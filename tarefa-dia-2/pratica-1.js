d854956fcd845b691d23d41907f766742cc8e863
Michele Ortega de Morais <micheleortega1@hotmail.com>
2022-08-24 15:45:38 -0300

    atualizacao

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,11 @@
-let nome = Michele
-let email = micheleortega1@hotmail.com
-let profissao = Desenvolvedora
-let idade = 32
-let sonho = Maldivas
+let nome = "Michele"
+let email = "micheleortega1@hotmail.com"
+let profissao = "Desenvolvedora"
+let idade = "32"
+let sonho = "Maldivas"
 
-consoloe.log (nome)
-console.log (email)
-console.log (profissao)
-console.log (idade)
-console.log (sonho)
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(sonho)
